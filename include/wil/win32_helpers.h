2ffc05920af33eabbdada4fd826a246812ace3ab
Chris Guzak (WINDOWS) <chrisg@ntdev.microsoft.com>
2021-03-28 16:31:57 -0700

    default to current process name on QueryFullProcessImageName

--- include/wil/win32_helpers.h
+++ include/wil/win32_helpers.h
@@ -438,7 +438,7 @@ namespace wil
     }
 
     template <typename string_type = wil::unique_cotaskmem_string, size_t stackBufferLength = 256>
-    string_type QueryFullProcessImageNameW(HANDLE processHandle, DWORD flags = 0)
+    string_type QueryFullProcessImageNameW(HANDLE processHandle = GetCurrentProcess(), DWORD flags = 0)
     {
         string_type result;
         THROW_IF_FAILED((wil::QueryFullProcessImageNameW<string_type, stackBufferLength>(processHandle, flags, result)));
