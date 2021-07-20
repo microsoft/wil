164c6de40a5359b40c093a209a1ee5aa25c5e455
Chandra Srinivasan <chsriniv@microsoft.com>
2021-07-20 10:38:19 -0700

    Update the license/copyright in TraceLoggingConfig.h

--- include/wil/traceloggingconfig.h
+++ include/wil/traceloggingconfig.h
@@ -1,8 +1,15 @@
 #pragma once
+//*********************************************************
+//
+//    Copyright (c) Microsoft. All rights reserved.
+//    This code is licensed under the MIT License.
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+//    ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+//    TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+//    PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//
+//*********************************************************
 /* ++
-
-Copyright (c) Microsoft Corporation.
-Licensed under the MIT License.
 
 Module Name:
 
