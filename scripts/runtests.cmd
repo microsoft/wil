55db00ef0ce019314d184f519fb95575e8b40080
Duncan Horn <dunhor@microsoft.com>
2022-12-07 22:08:19 -0800

    Getting address sanitizer to work with MSVC and compile with Clang

--- scripts/runtests.cmd
+++ scripts/runtests.cmd
@@ -59,6 +59,8 @@ call :execute_test noexcept witest.noexcept.exe
 if %ERRORLEVEL% NEQ 0 ( popd && goto :eof )
 call :execute_test normal witest.exe
 if %ERRORLEVEL% NEQ 0 ( popd && goto :eof )
+call :execute_test sanitize-address witest.asan.exe
+if %ERRORLEVEL% NEQ 0 ( popd && goto :eof )
 call :execute_test win7 witest.win7.exe
 if %ERRORLEVEL% NEQ 0 ( popd && goto :eof )
 popd
