5dce7a994a54425eedb20eab85b7e3965dc902c9
Ben Stolovitz <ben@stolovitz.com>
2023-04-17 22:35:04 -0700

    fix gcc compiler error

--- tests/RegistryTests.cpp
+++ tests/RegistryTests.cpp
@@ -38,14 +38,14 @@ const std::vector<std::wstring> multiStringTestArray[]{
 };
 
 const std::vector<BYTE> emptyStringTestValue{};
-const std::vector<BYTE> nonNullTerminatedString{ {'a'}, {0}, {'b'}, {0}, {'c'}, {0}, {'d'}, {0}, {'e'}, {0}, {'f'}, {0}, {'g'}, {0}, {'h'}, {0}, {'i'}, {0}, {'j'}, {0}, {'k'}, {0}, {'l'}, {0} };
+const std::vector<BYTE> nonNullTerminatedString{ 'a', 0, 'b', 0, 'c', 0, 'd', 0, 'e', 0, 'f', 0, 'g', 0, 'h', 0, 'i', 0, 'j', 0, 'k', 0, 'l', 0 };
 const std::wstring nonNullTerminatedStringFixed{ L"abcdefghijkl" };
 
 const std::vector<BYTE> vectorBytesTestArray[]
 {
-    { {0x00} },
+    { 0x00 },
     {},
-    { {0x1}, {0x2}, {0x3}, {0x4}, {0x5}, {0x6},{0x7}, {0x8}, {0x9},{0xa}, {0xb}, {0xc}, {0xd}, {0xe}, {0xf} }
+    { 0x1, 0x2, 0x3, 0x4, 0x5, 0x6,0x7, 0x8, 0x9,0xa, 0xb, 0xc, 0xd, 0xe, 0xf }
 };
 
 bool AreStringsEqual(const wil::unique_bstr& lhs, const std::wstring& rhs) noexcept
