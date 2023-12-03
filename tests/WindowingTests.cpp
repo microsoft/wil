e5ba6b25d300756cf8e0643eadff9b4636e2f2a8
Alexander Sklar <asklar@microsoft.com>
2023-12-02 23:50:45 -0800

    .

--- tests/WindowingTests.cpp
+++ tests/WindowingTests.cpp
@@ -4,15 +4,28 @@
 #if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP) && WIL_HAS_CXX_17
 TEST_CASE("EnumWindows", "[windowing]")
 {
+    // lambda can return a bool
     wil::for_each_window([](HWND hwnd)
         {
             REQUIRE(IsWindow(hwnd));
             return true;
         });
 
+    // or not return anything (we'll stop if we get an exception)
     wil::for_each_window([](HWND hwnd)
         {
             REQUIRE(IsWindow(hwnd));
+        });
+
+    // With captures
+    const auto pid = GetCurrentProcessId();
+    wil::for_each_window([pid](HWND hwnd)
+        {
+            if (pid == GetWindowThreadProcessId(hwnd, nullptr))
+            {
+                REQUIRE(IsWindow(hwnd));
+            };
+            return true;
         });
 
     const auto thread_id = GetCurrentThreadId();
@@ -27,6 +40,7 @@ TEST_CASE("EnumWindows", "[windowing]")
             REQUIRE(IsWindow(hwnd));
         });
 
+    // lambda can also return an HRESULT and we'll stop if it's not S_OK
     wil::for_each_thread_window(thread_id, [](HWND hwnd)
         {
             REQUIRE(IsWindow(hwnd));
