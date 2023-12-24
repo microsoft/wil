bff95a5502de0551cba95c4696624c048c1aae8e
Alexander Sklar <asklar@microsoft.com>
2023-12-23 22:23:39 -0800

    simplify wrappers, add child window, can work w/mutable lambdas

--- include/wil/windowing.h
+++ include/wil/windowing.h
@@ -12,67 +12,116 @@
 #define __WIL_WINDOWING_INCLUDED
 
 #include <WinUser.h>
+#include <exception>
 
 #if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
 namespace wil
 {
     namespace details
     {
-        template<typename TCallback>
-        BOOL CALLBACK LambdaEnumWindowsProc(HWND data, LPARAM lParam) noexcept
+        template<typename T> struct always_false : wistd::false_type {};
+
+        template<typename TEnumApi, typename TCallback>
+        void CallCallback(TEnumApi&& enumApi, TCallback&& callback)
         {
-            const TCallback* pCallback = reinterpret_cast<const TCallback*>(lParam);
-            using result_t = decltype((*pCallback)(data));
+            struct {
+                std::exception_ptr exception;
+                TCallback* pCallback;
+            } callbackData = { nullptr, &callback };
+            auto enumproc = [](HWND hwnd, LPARAM lParam) -> BOOL
+                {
+                    auto pCallbackData = reinterpret_cast<decltype(&callbackData)>(lParam);
+                    try {
+                        auto pCallback = pCallbackData->pCallback;
+                        using result_t = decltype((*pCallback)(hwnd));
 #ifdef __cpp_if_constexpr
-            if constexpr (std::is_void_v<result_t>)
-            {
-                try
-                {
-                    (*pCallback)(data);
-                    return TRUE;
-                }
-                catch (...)
-                {
-                    return FALSE;
-                }
-            }
-            else if constexpr (std::is_same_v<result_t, HRESULT>)
-            {
-                return (S_OK == (*pCallback)(data)) ? TRUE : FALSE;
-            }
-            else
+                        if constexpr (std::is_void_v<result_t>)
+                        {
+                            (*pCallback)(hwnd);
+                            return TRUE;
+                        }
+                        else if constexpr (std::is_same_v<result_t, HRESULT>)
+                        {
+                            // NB: this works for both HRESULT and NTSTATUS as both S_OK and ERROR_SUCCESS are 0
+                            return (S_OK == (*pCallback)(hwnd)) ? TRUE : FALSE;
+                        }
+                        else if constexpr (std::is_same_v<result_t, bool>)
+                        {
+                            return (*pCallback)(hwnd) ? TRUE : FALSE;
+                        }
+                        else
+                        {
+                            static_assert(details::always_false<TCallback>::value, "Callback must return void, bool, or HRESULT");
+                        }
+#else
+                        return (*pCallback)(hwnd);
 #endif
+                    }
+                    catch (...)
+                    {
+                        pCallbackData->exception = std::current_exception();
+                        return FALSE;
+                    }
+                };
+            enumApi(enumproc, reinterpret_cast<LPARAM>(&callbackData));
+            if (callbackData.exception)
             {
-                return (*pCallback)(data);
+                std::rethrow_exception(callbackData.exception);
             }
         }
-
-        template<typename TEnumWindows, typename TCallback>
-        auto for_each_window(const TEnumWindows& enumWindows, const TCallback& callback) noexcept
-        {
-            return enumWindows(LambdaEnumWindowsProc<TCallback>, reinterpret_cast<LPARAM>(&callback));
-        }
-
-        using EnumThreadWindowsType = decltype(&EnumThreadWindows);
-
-        template<typename TCallback>
-        auto for_each_thread_window(_In_ EnumThreadWindowsType enumThreadWindows, _In_ DWORD threadId, const TCallback& callback) noexcept
-        {
-            return enumThreadWindows(threadId, details::LambdaEnumWindowsProc<TCallback>, reinterpret_cast<LPARAM>(&callback));
-        }
     }
 
     template<typename TCallback>
-    auto for_each_window(TCallback&& callback) noexcept
+    auto for_each_window_nothrow(TCallback&& callback) noexcept
     {
-        return details::for_each_window(&EnumWindows, wistd::forward<TCallback>(callback));
+        details::CallCallback(&EnumWindows, wistd::forward<TCallback>(callback));
     }
 
     template<typename TCallback>
-    auto for_each_thread_window(_In_ DWORD threadId, TCallback&& callback) noexcept
+    auto for_each_window(TCallback&& callback)
     {
-        return details::for_each_thread_window(&EnumThreadWindows, threadId, wistd::forward<TCallback>(callback));
+        details::CallCallback(&EnumWindows, wistd::forward<TCallback>(callback));
+    }
+
+    template<typename TCallback>
+    auto for_each_thread_window_nothrow(_In_ DWORD threadId, TCallback&& callback) noexcept
+    {
+        auto boundEnumThreadWindows = [threadId](WNDENUMPROC enumproc, LPARAM lParam) noexcept -> BOOL
+            {
+                return EnumThreadWindows(threadId, enumproc, lParam);
+            };
+        details::CallCallback(boundEnumThreadWindows, wistd::forward<TCallback>(callback));
+    }
+
+    template<typename TCallback>
+    auto for_each_thread_window(_In_ DWORD threadId, TCallback&& callback)
+    {
+        auto boundEnumThreadWindows = [threadId](WNDENUMPROC enumproc, LPARAM lParam) noexcept -> BOOL
+            {
+                return EnumThreadWindows(threadId, enumproc, lParam);
+            };
+        details::CallCallback(boundEnumThreadWindows, wistd::forward<TCallback>(callback));
+    }
+
+    template<typename TCallback>
+    auto for_each_child_window_nothrow(_In_ HWND hwndParent, TCallback&& callback) noexcept
+    {
+        auto boundEnumChildWindows = [hwndParent](WNDENUMPROC enumproc, LPARAM lParam) noexcept -> BOOL
+            {
+                return EnumChildWindows(hwndParent, enumproc, lParam);
+            };
+        details::CallCallback(boundEnumChildWindows, wistd::forward<TCallback>(callback));
+    }
+
+    template<typename TCallback>
+    auto for_each_child_window(_In_ HWND hwndParent, TCallback&& callback)
+    {
+        auto boundEnumChildWindows = [hwndParent](WNDENUMPROC enumproc, LPARAM lParam) noexcept -> BOOL
+            {
+                return EnumChildWindows(hwndParent, enumproc, lParam);
+            };
+        details::CallCallback(boundEnumChildWindows, wistd::forward<TCallback>(callback));
     }
 }
-#endif
-#endif
+#endif // WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+#endif // __WIL_WINDOWING_INCLUDED
