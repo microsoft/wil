b0a0770aff6c63c578b7f75245386d547625f72f
Duncan Horn <dunhor@microsoft.com>
2025-12-10 13:28:08 -0800

    Use vswhere to find the latest VS install

--- scripts/call-vcvars.cmd
+++ scripts/call-vcvars.cmd
@@ -1,17 +1,16 @@
 @echo off
 
-:: NOTE: Intentionally not specifying 'setlocal' as we want the side-effects of calling 'vcvars' to affect the caller
+REM NOTE: Intentionally not specifying 'setlocal' as we want the side-effects of calling 'vcvars' to affect the caller
 
-:: NOTE: This is primarily intended to be used by the build pipelines, hence the hard-coded paths, and might not be
-::       generally useful. The following check is intended to help diagnose such possible issues
-if NOT EXIST "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat" (
-    echo ERROR: Could not locate 'vcvars' batch file. This script is intended to be run from a build machine & exit /B 1
-)
+set _VSWHERE=
+for /f "delims=" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -products * -latest -find VC\Auxiliary\Build\vcvarsall.bat') do set _VSWHERE=%%i
 
-set ARCH=%1
-if /I "%ARCH%"=="x86" (
+if "%_VSWHERE%"=="" echo ERROR: Unable to locate vcvarsall.bat & exit /B 1
+
+set _ARCH=%1
+if /I "%_ARCH%"=="x86" (
     REM Use the x64 toolchain
-    set ARCH=x64_x86
+    set _ARCH=x64_x86
 )
 
-call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %ARCH%
+call "%_VSWHERE%" %_ARCH%
